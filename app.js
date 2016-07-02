4aa1015e3c99b4a5dc95957437c82a960d2feea1
Anton Zuev <anzuev@bk.ru>
2016-07-02 20:28:03 +0300

    PSS-6 Pending review Assignee: RAisin

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ let app = express();
 
 SSO.init();
 
+
+app.set('etag', true);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
--- routes/upload/handlers/document.js
+++ routes/upload/handlers/document.js
@@ -2,5 +2,5 @@ var uploadFile = require('./../libs/uploadFile');
 
 
 module.exports = function(req, res, next){
-	uploadFile(req, res, "documentToBZ", {}, "public", next);
+	uploadFile(req, res, "uploadDocumentsToBZ", {}, "public", next);
 };
