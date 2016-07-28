f198fca299c660d945a64edfa7e1e597b09fa26d
Anton Zuev <anzuev@bk.ru>
2016-07-28 18:37:46 +0400

    Dev (#2)
    * 1.0.0(upload to test-server)
    
    * 1.0.0(upload to test-server)
    
    * #PSS-6
    
    * #PSS-6
    
    * fix uploading document

--- routes/handlers/sendFile.js
+++ routes/handlers/sendFile.js
@@ -5,7 +5,8 @@ var fs = require('fs');
 var HttpError = require('@anzuev/studcloud.errors').HttpError;
 let File = require(appRoot + '/models/File'),
 	mongoose = require("mongoose"),
-	Q = require('q');
+	Q = require('q'),
+	eTag = require('etag');
 let SSO = require("@anzuev/studcloud.sso");
 
 
@@ -14,28 +15,37 @@ module.exports = function(req, res, next) {
 
 	Q.async(function*() {
 
-
 		let file;
 		try {
 			let id = mongoose.Types.ObjectId(req.params.id);
-			file = yield File.getById(id);
+			file = yield File.getFileById(id);
 			if(!(yield SSO.checkPermissionToGetFile(req.user, req.params.id))) return next(404);
 		} catch (err) {
+			console.error(err);
 			return next(404);
 		}
+		let newEtag = eTag(file.path);
+		if(newEtag == req.headers['if-none-match']){
+			res.statusCode = 304;
+			res.end();
+		}else{
+			var fileStream = new fs.ReadStream(file.path);
+			res.setHeader('ETag', newEtag);
+			fileStream.pipe(res);
 
-		var fileStream = new fs.ReadStream(file.path);
-		fileStream.pipe(res);
+			fileStream
+				.on('error', function (err) {
+					console.log(err);
+					return next(new HttpError(404, "File not found", 404));
+				});
 
-		fileStream
-			.on('error', function (err) {
-				return next(new HttpError(404, "File not found", 404));
-			});
+			res
+				.on('close', function () {
+					fileStream.destroy();
+				});
+		}
 
-		res
-			.on('close', function () {
-				fileStream.destroy();
-			});
+
 
 	})().done();
 };
